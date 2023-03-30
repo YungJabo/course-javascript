78af65a0301f44f382d2c265c6e642f35e130548
Максим Кирсанов <kirsmaxi@gmail.com>
2023-03-30 13:56:39 +0300

    week-4

--- projects/loft-photo-lite-3/index.js
+++ projects/loft-photo-lite-3/index.js
@@ -1,8 +1,9 @@
 import pages from './pages.js';
 import mainPage from './mainPage.js';
 import loginPage from './loginPage.js';
-import('./styles.css');
+import './styles.css';
 
 pages.openPage('login');
+console.log('ok');
 loginPage.handleEvents();
 mainPage.handleEvents();
